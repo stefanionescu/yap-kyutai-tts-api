0aa42ac61b9a4a5485ea2b39058c7baff8657a4c
Stefan Ionescu <forgetthismail@protonmail.com>
2025-09-25 16:08:51 +0100

    350ms TTFB +/- 40ms with batch_size 24-32

--- docker/scripts/start_moshi.sh
+++ docker/scripts/start_moshi.sh
@@ -6,16 +6,21 @@ export LD_LIBRARY_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_confi
 
 # Reduce CPU thread contention and tokenizer overhead
 export CUDA_MODULE_LOADING=${CUDA_MODULE_LOADING:-EAGER}
-export RAYON_NUM_THREADS=${RAYON_NUM_THREADS:-1}
+export RAYON_NUM_THREADS=${RAYON_NUM_THREADS:-4}
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-1}
 export MKL_NUM_THREADS=${MKL_NUM_THREADS:-1}
 export TOKENIZERS_PARALLELISM=${TOKENIZERS_PARALLELISM:-false}
-export CUDA_DEVICE_MAX_CONNECTIONS=${CUDA_DEVICE_MAX_CONNECTIONS:-16}
+export CUDA_DEVICE_MAX_CONNECTIONS=${CUDA_DEVICE_MAX_CONNECTIONS:-64}
 
 # Performance optimizations
 export NVIDIA_TF32_OVERRIDE=${NVIDIA_TF32_OVERRIDE:-1}
 export TORCH_ALLOW_TF32_CUBLAS=${TORCH_ALLOW_TF32_CUBLAS:-1}
 export TORCH_ALLOW_TF32_CUDNN=${TORCH_ALLOW_TF32_CUDNN:-1}
+export RUST_LOG=${RUST_LOG:-info}
+
+# Reduce memory allocator and CUDA allocator jitter under concurrency
+export PYTORCH_CUDA_ALLOC_CONF=${PYTORCH_CUDA_ALLOC_CONF:-max_split_size_mb:128,expandable_segments:True}
+export MALLOC_ARENA_MAX=${MALLOC_ARENA_MAX:-2}
 
 uvx --from 'huggingface_hub[cli]' huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
 
